f7bca205f61e157ced04b0a9828d871c206275cb
Linzolle <linkziken99@gmail.com>
2018-03-17 22:54:53 -0500

    attempt to make bot restart on its own

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-from tsukino import Tsukino
+import asyncio
+import gc
+
+loops = 0
+max_wait_time = 60
 
 while True:
+    from tsukino import Tsukino
+    print("Connecting...")
     Tsukino().run()
+
+    asyncio.set_event_loop(asyncio.new_event_loop())
+    loops += 1
+
+    print("Cleaning up... ", end='')
+    gc.collect()
+    print("Done.")
+
+    sleeptime = min(loops * 2, max_wait_time)
+    if sleeptime:
+        print("Restarting in {} seconds...".format(loops*2))
+        time.sleep(sleeptime)
