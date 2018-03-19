27a2139f3f20b0a47e0deadda82116b3833f4891
Linzolle <linkziken99@gmail.com>
2018-03-18 21:45:08 -0500

    cleanup after running

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import asyncio
+import gc
+
 from tsukino import Tsukino
 
 Tsukino().run()
+
+asyncio.set_event_loop(asyncio.new_event_loop())
+
+print("Cleaning up... ", end='')
+gc.collect()
+print("Done.")
